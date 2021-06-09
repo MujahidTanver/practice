71bbeaf383f3a55a2f0c57ccf09b3a2de27eeb2f
MujahidTanver <mozahidul.islam07@gmail.com>
2021-06-10 00:34:51 +0600

    fisrt commit

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<?php
+ $sports='cricket';
+ 
+ switch($sports){
+
+    case('cricket'):
+        echo 'i love cricket';
+        break;
+    case('football'):
+        echo 'I love football';
+        break;
+    case ('hocky'):
+        echo 'I love hocky';
+        break;
+        default:
+        echo 'Valid input';
+
+ }
+
+?>
